334a0a3c48fecb58ba9195a94a38fe365059c6ae
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-11 19:03:55 +0530

    First commit

---
+++ src/main/kotlin/com/hrv/mart/backendorder/BackendOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.backendorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendOrderApplication>(*args)
+}
