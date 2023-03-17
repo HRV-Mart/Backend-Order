81b3f1cf9c40b09df7ee65887fea6f55d153c5ae
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-17 18:18:00 +0530

    Add detekt and jacoco (#10) (#11)
    * Add detekt (#10)
    
    * Added detekt configs
    
    * Added detekt
    
    * Add jacoco (#12)
    
    * Added jacoco
    
    * Updated unit test to pass build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.4"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("io.gitlab.arturbosch.detekt") version "1.22.0"
+	id("jacoco")// This is to use Jacoco for coverage testing
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 }
@@ -24,8 +26,13 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	// Detekt Formatting
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
 }
-
+detekt {
+	toolVersion = "1.22.0"
+	config = files("config/detekt/detekt.yml")
+}
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
@@ -35,4 +42,24 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	// To run Jacoco Test Coverage Verification
+	finalizedBy("jacocoTestCoverageVerification")
+}
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			excludes = listOf(
+				""
+			)
+			limit {
+				minimum = "0.9".toBigDecimal()
+			}
+		}
+	}
+}
+tasks.jacocoTestReport{
+	reports {
+		html.required.set(true)
+		generate()
+	}
 }
--- src/test/kotlin/com/hrv/mart/backendorder/BackendOrderApplicationTests.kt
+++ src/test/kotlin/com/hrv/mart/backendorder/BackendOrderApplicationTests.kt
@@ -8,6 +8,7 @@ class BackendOrderApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		main(emptyArray())
 	}
 
 }
