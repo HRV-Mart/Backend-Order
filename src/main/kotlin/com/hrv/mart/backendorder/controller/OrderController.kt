0f55dd0d73c93a6e26a3f8f1cc44f725e72af0aa
Harsh3305 <harshverma3305@gmail.com>
2023-04-05 12:35:45 +0530

    Added post mapping to save orders

--- src/main/kotlin/com/hrv/mart/backendorder/controller/OrderController.kt
+++ src/main/kotlin/com/hrv/mart/backendorder/controller/OrderController.kt
@@ -1,9 +1,13 @@
 package com.hrv.mart.backendorder.controller
 
+import com.hrv.mart.backendorder.model.OrderRequest
 import com.hrv.mart.backendorder.service.OrderService
+import com.hrv.mart.cartresponse.model.CartResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,5 +20,7 @@ class OrderController (
     @GetMapping("/{userID}")
     fun getUserOrders(@PathVariable userID: String) =
         orderService.getUserOrders(userID)
-
+    @PostMapping
+    fun addCartToOrder(@RequestBody orderRequest: OrderRequest) =
+        orderService.addOrder(orderRequest)
 }
