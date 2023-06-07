88a755a61badbd504778274081d704724873d589
Harsh3305 <harshverma3305@gmail.com>
2023-06-07 13:35:04 +0530

    Created order repository and productOrdered repository

--- src/main/kotlin/com/hrv/mart/backendorder/repository/OrderRepository.kt
+++ src/main/kotlin/com/hrv/mart/backendorder/repository/OrderRepository.kt
@@ -1,11 +1,11 @@
 package com.hrv.mart.backendorder.repository
 
-import com.hrv.mart.orderlibrary.model.OrderResponse
+import com.hrv.mart.orderlibrary.model.order.Order
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
 @Repository
-interface OrderRepository: ReactiveMongoRepository<OrderResponse, String> {
-    fun findOrdersByUserId(userId: String): Flux<OrderResponse>
+interface OrderRepository: ReactiveMongoRepository<Order, String> {
+    fun findOrdersByUserId(userId: String): Flux<Order>
 }
