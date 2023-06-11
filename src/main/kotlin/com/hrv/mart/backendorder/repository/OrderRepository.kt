d77cab378055f3678d151792534d2ed666440f28
Harsh3305 <harshverma3305@gmail.com>
2023-06-11 15:28:17 +0530

    Added a funciton in repository

--- src/main/kotlin/com/hrv/mart/backendorder/repository/OrderRepository.kt
+++ src/main/kotlin/com/hrv/mart/backendorder/repository/OrderRepository.kt
@@ -1,11 +1,18 @@
 package com.hrv.mart.backendorder.repository
 
+import com.hrv.mart.orderlibrary.model.Status
 import com.hrv.mart.orderlibrary.model.order.Order
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import java.time.LocalDateTime
 
 @Repository
 interface OrderRepository: ReactiveMongoRepository<Order, String> {
     fun findOrdersByUserId(userId: String): Flux<Order>
+    fun findOrderByStatusInAndDateTimeOfOrderBetween(
+        status: List<Status>,
+        start: LocalDateTime,
+        end: LocalDateTime
+    ): Flux<Order>
 }
