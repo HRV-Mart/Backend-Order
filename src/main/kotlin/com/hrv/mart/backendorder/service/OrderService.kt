a3ad7eddc8d1b9b609a39a6f74e9af027567901e
Harsh3305 <harshverma3305@gmail.com>
2023-06-11 15:28:45 +0530

    Added a function in service

--- src/main/kotlin/com/hrv/mart/backendorder/service/OrderService.kt
+++ src/main/kotlin/com/hrv/mart/backendorder/service/OrderService.kt
@@ -4,13 +4,18 @@ import com.hrv.mart.backendorder.repository.OrderRepository
 import com.hrv.mart.backendorder.repository.ProductOrderedRepository
 import com.hrv.mart.orderlibrary.model.OrderRequest
 import com.hrv.mart.orderlibrary.model.OrderResponse
+import com.hrv.mart.orderlibrary.model.Status
 import com.hrv.mart.orderlibrary.model.order.Order
 import com.hrv.mart.orderlibrary.model.order.ProductOrdered
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 @Service
 class OrderService(
@@ -47,6 +52,17 @@ class OrderService(
                         )
                     }
             }
+//    fun temp() =
+//        orderRepository.findOrderByStatusIn(listOf(Status.PROCESS))
+    fun temp() : Flux<Order>{
+        val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH:mm:ss", Locale.ROOT)
+        return orderRepository.findOrderByStatusInAndDateTimeOfOrderBetween(
+            status = listOf(Status.PROCESS),
+            start = LocalDateTime.parse("2023-06-10:12:46:41", pattern),
+            end = LocalDateTime.parse("2023-06-11:14:46:41", pattern)
+        )
+    }
+
     fun addOrder(orderRequest: OrderRequest): Mono<String> {
         val order = Order.parseFrom(orderRequest)
         val productOrdered = ProductOrdered.parseFrom(orderRequest, order)
